4d902e4b96c586f448de52ce43c53e9b8cfaa91c
Koki Fukuda <ko.fu.dev@gmail.com>
2023-07-25 12:55:33 +0900

    Execute command

--- guardian/src/main.rs
+++ guardian/src/main.rs
@@ -1,3 +1,40 @@
+use log::info;
+use std::fs::{create_dir_all, File};
+use std::io::prelude::*;
+use std::process::{Command, Stdio};
+
+fn agree_eula() -> std::io::Result<()> {
+    create_dir_all("/tmp/m")?;
+
+    let mut out = File::create("/tmp/m/eula.txt")?;
+    write!(out, "eula=true")?;
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    agree_eula().unwrap();
+
+    loop {
+        let result = Command::new("java")
+            .args(["-jar", "/tmp/server.jar", "nogui"])
+            .current_dir("/tmp/m")
+            .stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+        if result.success() {
+            break;
+        }
+
+        info!(
+            "Minecraft server exitted abnormally (code: {}). Restarting...",
+            result.code().unwrap()
+        );
+    }
 }
