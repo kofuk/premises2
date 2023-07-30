dba1a35b1cc6acb30d3ee0b0c6bf3f4e955e89db
Koki Fukuda <ko.fu.dev@gmail.com>
2023-07-30 18:24:07 +0900

    guardian: Implement Rcon client

--- guardian/src/rcon.rs
+++ guardian/src/rcon.rs
@@ -38,7 +38,7 @@ where
     T: Read + Write,
 {
     transport: T,
-    cur_req_id: i32,
+    handle_long_resp: bool,
 }
 
 #[derive(Debug)]
@@ -52,11 +52,32 @@ impl<T> RconClient<T>
 where
     T: Read + Write,
 {
-    pub fn new(transport: T) -> Self {
+    pub fn new(transport: T, handle_long_resp: bool) -> Self {
         Self {
             transport,
-            cur_req_id: 0,
+            handle_long_resp,
         }
+    }
+
+    fn send_packet(&mut self, packet_type: i32, payload: &str) -> Result<()> {
+        let len = 4 + 4 + 2 + payload.len();
+        if len > 1456 {
+            return Err(Error::RequestTooLong);
+        }
+
+        let mut buf = Vec::<u8>::new();
+
+        buf.write_all(&(len as i32).to_le_bytes()).unwrap();
+        buf.write_all(&1i32.to_le_bytes()).unwrap();
+        buf.write_all(&packet_type.to_le_bytes()).unwrap();
+        buf.write_all(payload.as_bytes()).unwrap();
+        buf.write_all(&[0; 2]).unwrap();
+
+        if let Err(err) = self.transport.write_all(buf.as_slice()) {
+            return Err(Error::Transport(err));
+        };
+
+        Ok(())
     }
 
     fn recv_packet(&mut self) -> Result<RconPacket> {
@@ -79,7 +100,7 @@ where
             Ok(_) => (),
             Err(err) => return Err(Error::Transport(err)),
         }
-        let payload = buf[0..buf.len() - 2].to_vec();
+        let payload = payload[0..payload.len() - 2].to_vec();
 
         Ok(RconPacket {
             req_id,
@@ -88,26 +109,34 @@ where
         })
     }
 
-    fn send_packet(&mut self, packet_type: i32, payload: &str) -> Result<()> {
-        let len = 4 + 4 + 2 + payload.len();
-        if len > 1456 {
-            return Err(Error::RequestTooLong);
+    fn recv_string(&mut self) -> Result<String> {
+        match self.send_packet(100, "") {
+            Ok(_) => (),
+            Err(err) => return Err(err),
         }
 
-        let mut buf = Vec::<u8>::new();
+        let mut result = String::new();
+        loop {
+            let packet = match self.recv_packet() {
+                Ok(packet) => packet,
+                Err(err) => return Err(err),
+            };
 
-        buf.write_all(&(len as i32).to_le_bytes()).unwrap();
-        buf.write_all(&self.cur_req_id.to_le_bytes()).unwrap();
-        buf.write_all(&packet_type.to_le_bytes()).unwrap();
-        buf.write_all(payload.as_bytes()).unwrap();
-        buf.write_all(&[0; 2]).unwrap();
+            if packet.packet_type != 0 {
+                return Err(Error::Proto);
+            }
 
-        if let Err(err) = self.transport.write_all(buf.as_slice()) {
-            return Err(Error::Transport(err));
-        };
-        self.cur_req_id += 1;
+            let payload = String::from_utf8_lossy(&packet.payload);
+            if payload == "Unknown request 64" {
+                break;
+            }
 
-        Ok(())
+            result += &payload;
+
+            break;
+        }
+
+        Ok(result)
     }
 
     pub fn authenticate(&mut self, passwd: &str) -> Result<()> {
@@ -129,18 +158,74 @@ where
             Ok(())
         }
     }
+
+    pub fn execute(&mut self, cmd: &str) -> Result<String> {
+        if let Err(err) = self.send_packet(2, cmd) {
+            return Err(err);
+        }
+
+        if self.handle_long_resp {
+            self.recv_string()
+        } else {
+            match self.recv_packet() {
+                Ok(packet) => Ok(String::from_utf8_lossy(&packet.payload).into_owned()),
+                Err(err) => Err(err),
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::net::TcpStream;
+
+    struct MockTcpStream {
+        readable_buf: &'static [u8],
+        wrote_bytes: Vec<u8>,
+    }
+    impl Read for MockTcpStream {
+        fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+            self.readable_buf.read(buf)
+        }
+    }
+    impl Write for MockTcpStream {
+        fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+            self.wrote_bytes.write(buf)
+        }
+
+        fn flush(&mut self) -> io::Result<()> {
+            Ok(())
+        }
+    }
 
     #[test]
-    fn test() {
-        // Remove this test in the future because it depends on a real Minecraft server.
+    fn test_execute_success() {
+        let mut mock_stream = MockTcpStream {
+            #[rustfmt::skip]
+            readable_buf: &[
+                // auth
+                10, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0,
+                // command
+                14, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, b'f', b'u', b'g', b'a', 0, 0,
+            ],
+            wrote_bytes: vec![],
+        };
 
-        let mut client = RconClient::new(TcpStream::connect(("localhost", 25575)).unwrap());
-        client.authenticate("x").unwrap();
+        {
+            let mut client = RconClient::new(&mut mock_stream, false);
+            client.authenticate("x").unwrap();
+            assert_eq!("fuga", client.execute("hoge").unwrap());
+        }
+
+        #[rustfmt::skip]
+        assert_eq!(
+            mock_stream.wrote_bytes,
+            [
+                // auth
+                11, 0, 0, 0, 1, 0, 0, 0, 3, 0, 0, 0, b'x', 0, 0,
+                // command
+                14, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, b'h', b'o', b'g', b'e', 0, 0
+            ]
+        );
     }
 }
